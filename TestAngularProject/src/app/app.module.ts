8a1e5abf83f9fddfbdcd55291cbc4c4e4c2292db
Ali Yaylagul <aliyaylagul@hotmail.com>
2023-08-31 16:15:34 +0300

    StructureChange

--- TestAngularProject/src/app/app.module.ts
+++ TestAngularProject/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutUsComponent } from './Pages/about-us/about-us.component';
+import { CategoriesComponent } from './Pages/home/components/categories/categories.component';
+import { ProductListComponent } from './Pages/home/components/product-list/product-list.component';
+import { HeaderComponent } from './Pages/home/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    CategoriesComponent,
+    ProductListComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
