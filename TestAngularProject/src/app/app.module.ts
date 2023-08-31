27639754bbcb603063722eec89e5ac99157b2ab4
Ali Yaylagul <aliyaylagul@hotmail.com>
2023-08-31 14:42:43 +0300

    Added Components

--- TestAngularProject/src/app/app.module.ts
+++ TestAngularProject/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoriesComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
