4b609797e5f051e57e43480efce0586caffcd894
Ali Yaylagul <aliyaylagul@hotmail.com>
2023-09-21 15:22:17 +0300

    SomeChanges

--- Biography/src/app/app-routing.module.ts
+++ Biography/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ExpertiseComponent } from './pages/expertise/expertise.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 const routes: Routes = [
   {
@@ -9,6 +11,14 @@ const routes: Routes = [
   },
   {
     path: 'about-us', component: AboutUsComponent
+  },
+  {
+    path: 'expertise',
+    component: ExpertiseComponent
+  },
+  {
+    path: 'header',
+    component: HeaderComponent
   }
 ];
 
--- Biography/src/app/app.module.ts
+++ Biography/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { ExpertiseComponent } from './pages/expertise/expertise.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    HeaderComponent,
+    ExpertiseComponent
   ],
   imports: [
     BrowserModule,
