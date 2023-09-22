1494d79367db8ff00fda9f24322afa889245fc53
Ali Yaylagul <aliyaylagul@hotmail.com>
2023-09-23 02:01:13 +0300

    Mission Page Designs

--- Biography/src/app/app.module.ts
+++ Biography/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import {HttpClientModule} from '@angular/common/http';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +14,7 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ExpertiseComponent } from './pages/expertise/expertise.component';
 import { ExpertiseService } from './services/expertise.service';
 import { MissionsComponent } from './pages/missions/missions.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -25,7 +29,10 @@ import { MissionsComponent } from './pages/missions/missions.component';
     AppRoutingModule,
     HttpClientModule,
 
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+      BrowserAnimationsModule,
+      
+      MatButtonModule,MatCardModule
   ],
   providers: [ExpertiseService],
   bootstrap: [AppComponent]
