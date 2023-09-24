63d5cb5ca7cfd064d6d5a1384df21fd53a5da48c
Ali Yaylagul <aliyaylagul@hotmail.com>
2023-09-24 21:17:55 +0300

    Some Changes

--- Biography/src/app/pages/missions/missions.component.ts
+++ Biography/src/app/pages/missions/missions.component.ts
@@ -73,6 +73,17 @@ import { MissionsService } from 'src/app/services/missions.service';
       transition('* => *', [
         animate('1s')
       ]),
+    ]),
+
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'translateX(100%)' }))
+      ])
     ])
   ]
 })
